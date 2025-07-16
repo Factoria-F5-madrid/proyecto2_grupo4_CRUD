f972a20941f7a5643b2394c40fd48944c3b88b91
Yeder <ypimentel.tapia@gmail.com>
2025-07-16 13:20:40 +0200

    fix : Updated import path for AsyncSessionLocal after restructuring routes and database modules

--- backend/routes/users_routes.py
+++ backend/routes/users_routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from db.database import AsyncSessionLocal
+from backend.db.database import AsyncSessionLocal
 
 router = APIRouter()
 
