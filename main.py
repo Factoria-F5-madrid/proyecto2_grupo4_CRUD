aee1389e82dc232ac3c9e16e3ad2f6d24356da99
Yeder <ypimentel.tapia@gmail.com>
2025-07-16 12:49:44 +0200

    feat(main)Set up FastAPI application with root route and integrated users router with async DB session

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from backend.routes import users_routes
+
+from backend.db.database import AsyncSessionLocal
+from backend.models.user_models import User
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "¡Funciona!"}
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
+
+
+app.include_router(users_routes.router, prefix="/users", tags=["Users"])
