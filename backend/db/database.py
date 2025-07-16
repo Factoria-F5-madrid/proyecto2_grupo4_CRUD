352611e07f8199bbe1ee6a62e369326f9745b50b
Yeder <ypimentel.tapia@gmail.com>
2025-07-16 13:03:27 +0200

    feat: Moved database URL to .env file for improved security and environment configuration using python-dotenv

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql+asyncpg://postgres@localhost:5432/petland"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
