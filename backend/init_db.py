bc7d1f8cf0418f55069e619ae770f192e52ae155
MariaDunaeva1 <maria_dunaeva@yahoo.es>
2025-07-16 20:43:05 +0200

    added medicalHistory_models to init

--- backend/init_db.py
+++ backend/init_db.py
@@ -4,10 +4,12 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from backend.db.database import engine
 from backend.models.user_models import Base
+from backend.models.medicalHistory_models import Base as MedicalHistoryBase
 
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+        await conn.run_sync(MedicalHistoryBase.metadata.create_all)
         print('Tablas creadas correctamente')
 
 if __name__ == "__main__":
