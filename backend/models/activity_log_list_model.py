9c766119fbb48a14d1f483adb834f39bfe58f92f
bunty <bbunty2001@gmail.com>
2025-07-17 11:36:41 +0200

    update: added ForeingnKey import

--- backend/models/activity_log_list_model.py
+++ backend/models/activity_log_list_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, BigInteger, String, Text, TIMESTAMP, func
+from sqlalchemy import Column, Integer, String, TIMESTAMP, func, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
