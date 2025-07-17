08a144507207365a3ed1509bac367dac3b3f0288
MariaDunaeva1 <maria_dunaeva@yahoo.es>
2025-07-17 11:06:18 +0200

    chore: added table name to foreign key, changed to lowcase in the table name, deleted Index=True in pet_id

--- backend/models/medicalHistory_models.py
+++ backend/models/medicalHistory_models.py
@@ -4,10 +4,10 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class MedicalHistory(Base):
-    __tablename__ = "MedicalHistory"
+    __tablename__ = "medical_history"
 
     id = Column(Integer, primary_key=True, index=True) 
-    pet_id = Column(Integer, ForeignKey("pet_id"), nullable=False, index=True) 
+    pet_id = Column(Integer, ForeignKey("Pet.pet_id"), nullable=False) 
     date = Column(TIMESTAMP(timezone=False), nullable=False, default=func.current_timestamp())  
     type = Column(String(30), nullable=False) 
     description = Column(Text, nullable=False) 
@@ -15,3 +15,4 @@ class MedicalHistory(Base):
     __table_args__ = (
         Index("historial_medico_id_mascota_index", "pet_id"),
     )
+
