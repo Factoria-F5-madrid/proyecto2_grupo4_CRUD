46e25ceb75aa2e6386bfe590f439c03d9cf3c707
Yeder <ypimentel.tapia@gmail.com>
2025-07-17 12:46:07 +0200

    add enums PetTypeEnum to pet_model

--- backend/models/enums.py
+++ backend/models/enums.py
@@ -4,3 +4,9 @@ ServiceTypeEnum = Enum(
     'Guarderia', 'Transporte', 'Comida', 'Otros',
     name='service_type_enum'
 )
+
+PetTypeEnum = Enum(
+    'Canino', 'Felino', 'Reptil', 'Anfibio', 'Ave', 'Pez', 'Roedor', 'Otro',
+    name='pet_type_enum'
+)
+
--- backend/models/pet_model.py
+++ backend/models/pet_model.py
@@ -1,35 +1,20 @@
 from sqlalchemy import Column, Integer, String, Date, ForeignKey, CheckConstraint
 from sqlalchemy.ext.declarative import declarative_base
-from backend.models import Base  # usa el único Base común
+from backend.models.base_models import Base
+from .enums import PetTypeEnum
 
 class Pet(Base):
       __tablename__ = 'Pet'
 
       pet_id = Column(Integer, primary_key=True, nullable=False)
       name = Column(String(100), nullable=False)
-      species = Column(String(50), nullable=False)
+      species = Column(PetTypeEnum, nullable=False)
       breed = Column(String(100), nullable=False)
       birth_date = Column(Date, nullable=False)
       allergies = Column(String(100), nullable=False)
       special_needs = Column(String(100), nullable=False)
 
       client_id = Column(Integer, ForeignKey('User.client_id'), nullable=False)
-
-      __table_args__ = (
-        CheckConstraint(
-            species.in_([
-                'Caninos',
-                'Felinos',
-                'Reptiles',
-                'Anfibios',
-                'Aves',
-                'Peces',
-                'Roedores',
-                'Otro'
-            ]),
-            name='chk_species_type'
-        ),
-    )
 
       
       
