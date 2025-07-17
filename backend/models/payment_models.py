ec8efaa2cfc96bf02fe60568313b8e279769d6e8
Barbarasanchez11 <barbarasanchezurbano@gmail.com>
2025-07-17 13:27:40 +0200

    Refactor: applied Python Enum and SQLAlchemy Enum to Payment model for method and payment_status, removed CheckConstraints

---
+++ backend/models/payment_models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, DECIMAL, Boolean, TIMESTAMP, func
+from .base_models import Base
+from .enums import SqlPaymentMethodEnum, SqlPaymentStatusEnum
+
+
+
+class Payment(Base):
+    __tablename__ = "payment"
+
+    payment_id = Column(Integer, primary_key=True, nullable=False)
+    amount = Column(DECIMAL(10, 2), nullable=False)
+    method = Column(SqlPaymentMethodEnum, nullable=False, server_default="IoT_Devices")
+    payment_date = Column(TIMESTAMP(timezone=False), nullable=False, server_default=func.now())
+    payment_status = Column(SqlPaymentStatusEnum, nullable=False, server_default="Pending")
+    refund_return = Column(Boolean, nullable=False, server_default="false")
