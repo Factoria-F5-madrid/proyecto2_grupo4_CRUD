6b7542d9b2d1928f3a5e165eb9efc2fd97481ea1
Barbarasanchez11 <barbarasanchezurbano@gmail.com>
2025-07-18 11:31:24 +0200

    feat(service): add GET endpoint to retrieve all services

--- backend/routes/service_routes.py
+++ backend/routes/service_routes.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from typing import List
+
 
 from backend.schema.service_schemas import ServiceCreate, ServiceOut
 from backend.models.service_models import Service
@@ -17,3 +19,9 @@ async def create_service(service_data: ServiceCreate, db: AsyncSession = Depends
     await db.refresh(new_service)
     
     return new_service
+
+@router.get("/", response_model=List[ServiceOut])
+async def get_all_services(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Service))
+    services = result.scalars().all()
+    return services
