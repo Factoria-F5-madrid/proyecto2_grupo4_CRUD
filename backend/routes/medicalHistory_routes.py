ef2bc6238cd529c3c1003ec009b90d1d9eb4e318
MariaDunaeva1 <maria_dunaeva@yahoo.es>
2025-07-18 23:01:50 +0200

    chore: medicalHistory route changed to match to schema

--- backend/routes/medicalHistory_routes.py
+++ backend/routes/medicalHistory_routes.py
@@ -1,32 +1,27 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from pydantic import BaseModel
 from backend.db.database import AsyncSessionLocal
 from backend.models.medicalHistory_models import MedicalHistory
+from backend.schema.medicalHistory_schema import MedicalHistoryCreate, MedicalHistoryResponse
 
 router = APIRouter(prefix="/medical_history", tags=["MedicalHistory"])
-
-class MedicalHistoryCreate(BaseModel):
-    pet_id: int
-    type: str
-    description: str
 
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
 
-@router.get("/")
+@router.get("/", response_model=list[MedicalHistoryResponse])
 async def get_medical_histories(db: AsyncSession = Depends(get_db)):
     return {"message": "Ruta de historial médico funcionando"}
 
-@router.get("/list")
+@router.get("/list", response_model=list[MedicalHistoryResponse])
 async def list_medical_histories(db: AsyncSession = Depends(get_db)):
     result = await db.execute(select(MedicalHistory))
     histories = result.scalars().all()
     return histories
 
-@router.post("/")
+@router.post("/", response_model=list[MedicalHistoryResponse])
 async def create_medical_history(history: MedicalHistoryCreate, db: AsyncSession = Depends(get_db)):
     new_history = MedicalHistory(
         pet_id=history.pet_id,
