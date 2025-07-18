576d2402849ffe9c8a52b1d875da05415e26dbd9
Yeder <ypimentel.tapia@gmail.com>
2025-07-18 20:24:08 +0200

    feat(routes): integrate pet controller into pet routes

--- backend/routes/pet_route.py
+++ backend/routes/pet_route.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from db.database import AsyncSessionLocal
+from backend.controllers import pet_controller
+from backend.schema.pet_schema import PetSchema
 
 router = APIRouter()
 
@@ -10,23 +12,23 @@ async def get_db():
 
 @router.get("/")
 async def get_pets(db: AsyncSession = Depends(get_db)):
-    return {"message": "Ruta de mascotas funcionando"}
+    return await pet_controller.get_pets(db)
 
 #ahora hacer un get por id
 
 @router.get("/{id}")
 async def get_pets(db: AsyncSession = Depends(get_db)):
-    return {"message": "Ruta de mascotas funcionando"}
+     return await pet_controller.get_pets_by_id(db, pet_id=id)
 
 @router.post("/")
 async def create_pets(db: AsyncSession = Depends(get_db)):
-    return {"message": "Ruta de mascotas funcionando"}
+    return await pet_controller.create_pets(db, pet=PetSchema())
 
 @router.put("/")
 async def update_pets(db: AsyncSession = Depends(get_db)):
-    return {"message": "Ruta de mascotas funcionando"}
+    return await pet_controller.update_pets(db, pet_id=id, pet=PetSchema())
 
 @router.delete("/")
 async def delete_pets(db: AsyncSession = Depends(get_db)):
-    return {"message": "Ruta de mascotas funcionando"}
+    return await pet_controller.delete_pets(db, pet_id=id)
 
