6a69987f217d2607b1807089a3c0b1cf319f7d23
MariaDunaeva1 <maria_dunaeva@yahoo.es>
2025-07-21 14:26:00 +0200

    feat: add getReservation function to fetch reservations access via API; stylistic changes in josAssigmentServices

---
+++ frontend/src/services/reservationServices.js
@@ -0,0 +1,89 @@
+import axios from "axios";
+
+const BASE_URL = "http://127.0.0.1:8000/reservation"; 
+
+//Acceder a las reservas
+export const getReservations = async () => {
+  try {
+    const response = await axios.get(BASE_URL);
+    return response.data;
+  } catch (error) {
+    console.error("Error al acceder a las reservas:", error);
+    throw error;
+  }
+};
+
+// Obtener la lista de los historiales médicos
+export const getListMedicalHistories = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/list`);
+    return response.data;
+  } catch (error) {
+    console.error(`Error al obtener las lista de los historiales médicos:`, error);
+    throw error;
+  }
+};
+
+// Obtener un historial médico por ID
+export const getMedicalHistoryByID = async (id) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/list/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(`Error al obtener el historial médico con ID ${id}:`, error);
+    throw error;
+  }
+};
+
+// Crear un nuevo historial médico
+export const createMedicalHistory = async (formData) => {
+  try {
+    const response = await axios.post(
+      "http://127.0.0.1:8000/medicalHistory/medical_history/",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return response.data; 
+  } catch (error) {
+    console.error("Error al crear el Historial Médico:", error);
+    throw error; 
+  }
+};
+
+// Eliminar un historial médico
+export const deleteMedicalHistory = async (id) => {
+  const token = localStorage.getItem("token"); 
+
+  try {
+    const response = await axios.delete(`${BASE_URL}/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`, 
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(`Error al eliminar el Historial Médico con el id ${id}:`, error);
+    throw error;
+  }
+};
+
+// Actualizar el Historial Médical por ID
+export const updateMedicalHistory = async (id, updatedData) => {
+  const token = localStorage.getItem("token"); 
+
+  try {
+    const response = await axios.put(`${BASE_URL}/${id}`, updatedData, {
+      headers: {
+        Authorization: `Bearer ${token}`, 
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(`Error al actualizar el video con ID ${id}:`, error);
+    throw error;
+  }
+};
