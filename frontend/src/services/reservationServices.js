5f0c975343c901b04a7548e62360ea2854ca0ec6
MariaDunaeva1 <maria_dunaeva@yahoo.es>
2025-07-21 14:38:43 +0200

    feat: add getReservationByID function to fetch reservation by ID via API

--- frontend/src/services/reservationServices.js
+++ frontend/src/services/reservationServices.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = "http://127.0.0.1:8000/reservation"; 
 
 //Acceder a las reservas
-export const getReservations = async () => {
+export const getReservation = async () => {
   try {
     const response = await axios.get(BASE_URL);
     return response.data;
@@ -13,24 +13,24 @@ export const getReservations = async () => {
   }
 };
 
-// Obtener la lista de los historiales médicos
-export const getListMedicalHistories = async () => {
+// Obtener la lista de las reservas
+export const getListReservations = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/list`);
     return response.data;
   } catch (error) {
-    console.error(`Error al obtener las lista de los historiales médicos:`, error);
+    console.error(`Error al obtener las lista de las reservas:`, error);
     throw error;
   }
 };
 
-// Obtener un historial médico por ID
-export const getMedicalHistoryByID = async (id) => {
+// Obtener una reserva por ID
+export const getReservationByID = async (id) => {
   try {
     const response = await axios.get(`${BASE_URL}/list/${id}`);
     return response.data;
   } catch (error) {
-    console.error(`Error al obtener el historial médico con ID ${id}:`, error);
+    console.error(`Error al obtener la reserva con ID ${id}:`, error);
     throw error;
   }
 };
