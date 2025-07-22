20124e56f98ea0c453c0ec93f1c603cd5cc489e6
Yeder <ypimentel.tapia@gmail.com>
2025-07-22 12:39:11 +0200

    feat: add tabs navigation with routes

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -4,6 +4,14 @@ import Spinner from "../pages/Spinner";
 import Layout from "../layout/Layout";
 import Home from "../pages/Home";
 import ContactUs from "../pages/ContactUs";
+import Reservations from "../pages/Reservations";
+import Pets from "../pages/Pets";
+import MedicalHistory from "../pages/MedicalHistory";
+import Payments from "../pages/Payments";
+import Invoice from "../pages/Invoice";
+import Account from "../pages/Account";
+import Settings from "../pages/Settings";
+
 
 export const router = createBrowserRouter([
   {
@@ -21,8 +29,35 @@ export const router = createBrowserRouter([
       {
         path: "contact", // /contact-us
         element: <ContactUs />,
-      }
-
+      },
+      {
+        path: "reservations", // /reservations
+        element: <Reservations />,
+      },
+      {
+        path: "pets", // /pets
+        element: <Pets />,
+      },
+      {
+        path: "medicalhistory", // /medical-history
+        element: <MedicalHistory />,
+      },
+      {
+        path: "payments", // /payments
+        element: <Payments />,
+      },
+      {
+        path: "invoices", // /invoice
+        element: <Invoice />,
+      },
+      {
+        path: "account", // /account
+        element: <Account />,
+      },
+      {
+        path: "settings", // /settings
+        element: <Settings />,
+      },
       // aqui se agregan mas rutas chicos :) 
     ],
   },
